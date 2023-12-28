dd359e691c540aba1dbd0545199460631b8ce449
Jayesh Kharode <jayesh.kharode@technoforte.co.in>
2023-12-28 15:49:00 +0530

    fix test
    Signed-off-by: Jayesh Kharode <jayesh.kharode@technoforte.co.in>

--- admintest/entrypoint.sh
+++ admintest/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 java --version
+
+
+#!/bin/bash
+
+# Start Selenium Hub
+java -jar /opt/selenium/selenium-server-standalone.jar -role hub &
+sleep 5  # Wait for Selenium Hub to start
+
+# Start Selenium Node with Chrome
+java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar /opt/selenium/selenium-server-standalone.jar -role node -hub http://localhost:4444/grid/register -browser "browserName=chrome,maxInstances=5" &
+
+# Wait for a while to allow nodes to register
+sleep 10
+
+# Run your test or other application
 java -jar adminui-*-jar-with-dependencies.jar
